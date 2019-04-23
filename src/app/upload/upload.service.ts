7cdeff3be180d61c2120e7b02b400c95b6951885
Omri Shtamberger <omrishtam@gmail.com>
2019-04-23 13:03:43 +0300

    upload file in a single request
    Signed-off-by: Omri Shtamberger <omrishtam@gmail.com>

--- src/app/upload/upload.service.ts
+++ src/app/upload/upload.service.ts
@@ -68,72 +68,29 @@ export class UploadService {
         this.http.request<string>(initReq).subscribe(initEvent => {
           if (initEvent instanceof HttpResponse) {
             const uploadId = initEvent.body;
-            let chunkSize = (file.size / 50) | 0;
-            if (chunkSize < 5 << 20) { // Min chunkSize is 5MB
-              chunkSize = 5 << 20;
-            }
 
-            if (chunkSize > 5000 << 20) { // Max chunkSize is 5GB
-              chunkSize = 5000 << 20;
-            }
+            const formData: FormData = new FormData();
+            formData.append("file", file, file.name);
 
-            let uploadedBytes = 0;
-            let totalChunks = (file.size / chunkSize) | 0;
-            if (file.size % chunkSize != 0) {
-              totalChunks++;
-            }
+            const req = new HttpRequest("POST", `${url}?uploadType=resumable&uploadId=${uploadId}`, formData, {
+              reportProgress: true,
+              responseType: 'text'
+            });
 
-            const chunksRequests: Observable<HttpEvent<{}>>[] = [];
-            for (let chunkIndex = 0; chunkIndex < totalChunks; chunkIndex++) {
-              // create a new multipart-form for every file part
-              const chunkStart = chunkIndex * chunkSize;
-              let chunkEnd = chunkStart + chunkSize;
-              if (chunkEnd > file.size) {
-                chunkEnd = file.size;
+            // send the http-request
+            // send the http-request and subscribe for progress-updates
+            this.http.request(req).subscribe(event => {
+              if (event.type === HttpEventType.UploadProgress) {
+                // calculate the progress percentage
+
+                const percentDone = Math.round((100 * event.loaded) / event.total);
+                // pass the percentage into the progress-stream
+                progress.next(percentDone);
+              } else if (event instanceof HttpResponse) {
+                // Close the progress-stream if we get an answer form the API
+                // The upload is complete
+                progress.complete();
               }
-
-              const part = file.slice(chunkStart, chunkEnd, file.type);
-              const formData: FormData = new FormData();
-              formData.append("file", part, file.name);
-              formData.append('uploadId', uploadId);
-              formData.append('chunkIndex', `${chunkIndex + 1}`);
-
-              const partReq = new HttpRequest("POST", `${url}?uploadType=resumable`, formData, {
-                reportProgress: true,
-                responseType: 'text'
-              });
-
-              // send the http-request
-              const chunkReqObservable = this.http.request(partReq).pipe(concatMap(event => {
-                  if (event.type === HttpEventType.UploadProgress && event.loaded === event.total) {
-                    uploadedBytes += event.loaded;
-                    // calculate the progress percentage
-    
-                    const percentDone = Math.round((100 * uploadedBytes) / file.size);
-                    // pass the percentage into the progress-stream
-                    progress.next(percentDone);
-                  }
-
-                  return of(event);
-              }));
-
-              chunksRequests.push(chunkReqObservable);
-            }
-
-            forkJoin(chunksRequests).subscribe(events => {
-                const completeReqHeaders = new HttpHeaders().set('Content-Disposition', `filename=${file.name}`)
-                const completeReq = new HttpRequest("PUT", `${url}?uploadId=${uploadId}`, undefined, {
-                  responseType: 'text',
-                  headers: completeReqHeaders
-              });
-
-              this.http.request(completeReq).subscribe(event => {
-                if (event instanceof HttpResponse) {
-                  // Close the progress-stream if we get an answer form the API
-                  // The upload is complete
-                  progress.complete();
-                }
-              });
             });
           }
         });
