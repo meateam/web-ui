d20466917fde0ab0058bd6959e22c7bf16357e34
Omri Shtamberger <omrishtam@gmail.com>
2019-08-06 13:22:49 +0300

    add updating of configuration from /api/config and updated baseURL for k8s
    Signed-off-by: Omri Shtamberger <omrishtam@gmail.com>

--- src/main.js
+++ src/main.js
@@ -1,23 +1,38 @@
-import { sync } from 'vuex-router-sync'
-import store from '@/store'
-import router from '@/router'
-import i18n from '@/i18n'
-import Vue from '@/utils/vue'
-import { validateLogin } from '@/utils/auth'
-import App from '@/App'
+import { sync } from 'vuex-router-sync';
+import store from '@/store';
+import router from '@/router';
+import i18n from '@/i18n';
+import Vue from '@/utils/vue';
+import { validateLogin } from '@/utils/auth';
+import App from '@/App';
+import { baseURL } from '@/utils/constants';
+import config from './utils/config';
 
-sync(store, router)
+export const localStorageConfigKey = 'config';
+sync(store, router);
 
 async function start () {
-    await validateLogin()
-    new Vue({
-    el: '#app',
-    store,
-    router,
-    i18n,
-    template: '<App/>',
-    components: { App }
-  })
+	let conf = localStorage.getItem(localStorageConfigKey);
+	if (conf) {
+		conf = JSON.parse(conf);
+	} else {
+		const res = await fetch(`${baseURL}/api/config`);
+		conf = await res.json();
+		localStorage.setItem(localStorageConfigKey, JSON.stringify(conf));
+	}
+
+	config.apmServerUrl = conf.apmServerUrl;
+	config.environment = conf.environment;
+
+	await validateLogin();
+	new Vue({
+		el: '#app',
+		store,
+		router,
+		i18n,
+		template: '<App/>',
+		components: { App }
+	});
 }
 
-start()
+start();
