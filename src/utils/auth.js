0bea7e97bd882cc8313333d3fca7d35074800367
Omri Shtamberger <omrishtam@gmail.com>
2019-08-06 15:24:02 +0300

    auth works on k8s
    Signed-off-by: Omri Shtamberger <omrishtam@gmail.com>

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -66,7 +66,7 @@ export async function validateLogin () {
     if (localStorage.getItem('jwt') || getCookie("kd-token")) {
       parseToken();
     } else {
-      window.open(`http://localhost:8084/auth/login`, '_self');
+      window.open(`/auth/login`, '_self');
     }
 
   } catch (_) {
