f8e4e6d9fdc035ea748e240b5b27a05d0a9114d1
AllKinds <yonatald@gmail.com>
2019-08-19 11:34:03 +0200

    Feature/contact us (#58)
    * add contact-us button with a link from the api
    
    Signed-off-by: Yonatan <yonatald@gmail.com>

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -13,6 +13,12 @@
           <span>{{ $t('sidebar.newFolder') }}</span>
         </button>
       </div>
+      <div>
+        <a v-bind:href = supportLink class="action" :aria-label="$t('sidebar.contactUs')" :title="$t('sidebar.contactUs')">
+          <i class="material-icons">headset_mic</i>
+          <span>Contact Us</span>
+        </a>
+      </div>
 
     </template>
 
@@ -21,7 +27,6 @@
         <span>Beta version</span>
         <span> {{ version }}</span>
       </span>
-      <span>Enter link to support</span>
     </p>
   </nav>
 </template>
@@ -29,7 +34,7 @@
 <script>
 import { mapState, mapGetters } from 'vuex'
 import * as auth from '@/utils/auth'
-import { version, signup, disableExternal, noAuth } from '@/utils/constants'
+import { version, signup, disableExternal, noAuth, config } from '@/utils/constants'
 
 export default {
   name: 'sidebar',
@@ -40,6 +45,7 @@ export default {
       return this.$store.state.show === 'sidebar'
     },
     signup: () => signup,
+    supportLink: () => config.supportLink,
     version: () => version,
     disableExternal: () => disableExternal,
     noAuth: () => noAuth,
