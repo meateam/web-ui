91914452998cfce5e8ca24ee5f8fcb74c898b7f3
shahar-y <shya95@gmail.com>
2020-06-18 16:51:53 +0300

    fixed bug for file in root then upload resumable to folder
    Signed-off-by: shahar-y <shya95@gmail.com>

--- src/api/files.js
+++ src/api/files.js
@@ -135,7 +135,7 @@ export async function upload(url, file, headers, onupload) {
 export async function uploadInit(file) {
 	return new Promise((resolve, reject) => {
 		const fileMetadata = { title: file.name, mimeType: file.type };
-		const url = `${baseURL}/api/upload`;
+		const url = `${baseURL}/api/upload?parent=${file.parent}`;
 		const request = new XMLHttpRequest();
 		const responseUploadIdHeader = 'x-uploadid';
 		request.open('POST', url, true);
@@ -168,10 +168,12 @@ export async function post(base, file, onupload) {
 		let headers = {
 			Authorization: 'Bearer ' + store.state.jwt
 		}
-
+		file.parent = base;
 		if (file.size <= 5 << 20) {
 			url += '?uploadType=multipart';
 		} else {
+					// eslint-disable-next-line
+					console.log(file);
 			const uploadId = await this.uploadInit(file);
 			headers['Content-Range'] = `bytes 0-${file.size - 1}/${file.size}`;
 			url += `?uploadType=resumable&${requestUploadIdHeader}=${uploadId}`;
